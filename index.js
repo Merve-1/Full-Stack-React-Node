9d89fb5ccaf772642852a0d099b2e709a0966f63
Merve-1 <marwa.alazaab@gmail.com>
2026-08-11 15:00:42 +0300

    Adding Server Setup:

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 //file system module
 const fs = require('fs');
+//http module
+const http = require('http');
 
 const hello = 'Hello World';
 console.log(hello);
 
+//Read file synchronously
 const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 console.log(textIn);
 
+
+//Server
+// const server = http.createServer((req, res) => {
+//     console.log(`Someone connected from IP: ${req.socket.remoteAddress}`);
+
+//     res.end('Hello from the server');
+// });
+
+const server = http.createServer((req, res) => {
+    console.log(
+        `Request: ${req.method} ${req.url} | IP: ${req.socket.remoteAddress}`
+    );
+
+    res.end('Hello from the server');
+});
+
+server.listen(8000, '0.0.0.0', () => {
+    console.log('Listening to requests on port 8000');
+});
