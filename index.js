a1dae145fe80b2f8dfa0e2a4f8bca3d9045cfb96
Merve-1 <marwa.alazaab@gmail.com>
2026-08-11 15:49:15 +0300

    manipulating json files

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const fs = require('fs');
 //http module
 const http = require('http');
+//url module
+const url = require('url');
 
 const hello = 'Hello World';
 console.log(hello);
@@ -20,10 +22,37 @@ console.log(textIn);
 
 const server = http.createServer((req, res) => {
     console.log(
-        `Request: ${req.method} ${req.url} | IP: ${req.socket.remoteAddress}`
+        `Request: ${req.method} ${req.url} | Port: ${req.socket.remotePort} | Remote Address: ${req.socket.remoteAddress} | Status: ${res.statusCode}`
     );
+    // res.end('Hello from the server');
 
-    res.end('Hello from the server');
+    const pathName = req.url;
+    if (pathName === '/' || pathName === '/overview') {
+        res.end('This is the OVERVIEW');
+    }else if (pathName === '/product') {
+        res.end('This is the PRODUCT');
+    }else if(pathName === '/api'){
+        fs.readFile('./data.json', 'utf-8', (err, data) => {
+            if(err) {
+                res.writeHead(500, {
+                    'Content-type': 'text/html',
+                    'my-own-header': 'hello-world'
+                });
+                res.end('<h1>Internal Server Error</h1>');
+            }
+            res.writeHead(200, {
+                'Content-type': 'application/json',
+                'my-own-header': 'hello-world'
+            });
+            res.end(data);
+        });
+    }else{
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+        res.end('<h1>Page not found!</h1>');
+    }
 });
 
 server.listen(8000, '0.0.0.0', () => {
